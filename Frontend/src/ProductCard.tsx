a4d5b8ecef4af62ab370e20f017476d13a9f0a6e
sibusiso sithole <ngwevu.15075@gmail.com>
2025-09-23 17:43:20 +0200

    Update ProductCard.tsx

--- Frontend/src/ProductCard.tsx
+++ Frontend/src/ProductCard.tsx
@@ -9,7 +9,7 @@ export default function ProductCard({ product }: ProductCardProps) {
     <div className="group cursor-pointer">
       <div className="aspect-square bg-gray-50 rounded-lg overflow-hidden mb-3 group-hover:shadow-lg transition-shadow">
         <img
-          src={product.image}
+          src= "https://github.com/Sibusiso15075/Bespoke-Website/blob/b860fd8c771383d0a0d5b18c576abc19163b8ad8/Frontend/public/525051649_17846033274536509_6193107844236981155_n.jpg"
           alt={product.name}
           className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
         />
